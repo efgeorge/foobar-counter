4d22d4280d306275e691a1669a313e19b00e41a2
efgeorge <fgeorge.enache@gmail.com>
2023-09-04 15:38:04 +0300

    app is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const decreaseHandler = () => {
+    if (count > 0) {
+      setCount(count -1);
+    }
+    return null;
+  };
+  const increaseHandler = () => {
+    setCount(count + 1);
+  }
+  let content;
+  if (count%2 === 0 && count%5 === 0) {
+    content = "FooBar";
+  } else if (count%2 === 0) {
+    content = "Foo";
+  } else if (count%5 === 0) {
+    content = "Bar";
+  } else {
+    content = count
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="counter">
+        {count}
+      </h1>
+      <div className="content-wrapper">
+        <button onClick={decreaseHandler} className="button">-</button>
+        <p className="content">{content}</p>
+        <button onClick={increaseHandler} className="button">+</button>
+      </div>
     </div>
   );
 }
